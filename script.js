4019e2be90d9bea34a933ef5ccf002261bff8302
ISHIMWE Ami Paradis <141851169+amiparadis250@users.noreply.github.com>
2024-03-02 12:30:10 +0200

    Files arrangement fixed

--- script.js
+++
@@ -1,9 +0,0 @@
-const hamMenu = document.querySelector('.ham-menu');
-const navMenuu = document.querySelector('navigation-menu');  
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    navMenuu.classList.toggle('active'); 
-});
-
-
-
